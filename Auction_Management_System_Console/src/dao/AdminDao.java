4decaab564539538fceb856170fc0d73c4ae28c1
Kali Rishi K <110583211+kalirishik@users.noreply.github.com>
2025-10-03 16:42:18 +0530

    Delete Auction_Management_System_Console directory

--- Auction_Management_System_Console/src/dao/AdminDao.java
+++
@@ -1,6 +0,0 @@
-package dao;
-import exception.AdminException;
-public interface AdminDao {
-    public boolean checkAdmin(String e,String p) throws AdminException;
-    public void AdminHomeWindow() throws AdminException;
-}
--- Auction_Management_System_Console/src/exception/AdminException.java
+++
@@ -1,9 +0,0 @@
-package exception;
-
-public class AdminException extends Exception{
-    public AdminException() {
-    }
-    public AdminException(String message) {
-        super(message);
-    }
-}
